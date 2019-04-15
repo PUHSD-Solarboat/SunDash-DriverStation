bbd9756c5ae43fb0a253c24f489bd9f0e8932c04
Jatnael Montes <293746@my.puhsd.org>
2019-04-15 11:46:56 -0700

    Git Test

--- testingLED.ino
+++ testingLED.ino
@@ -1,5 +1,5 @@
 /*
-Sample code to test out LEDs (to see if they still work)
+Sample code to test out LEDs (to see if they still work.)
 
 Update [04.15.2019]: After taking the LEDs outside, it is too hard to determine when 
 the LED is on/off. Therefore, we are taking a different approach to alert the driver when to 
